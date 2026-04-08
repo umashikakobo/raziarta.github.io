9ec501142a6b22df7294a90ced8ebf5d0563b13f
raziarta <raziarta7716@gmail.com>
2026-04-08 21:53:19 +0900

    Delete uma-shika-portal directory

--- uma-shika-portal/main.js
+++
@@ -1,7 +0,0 @@
-// 馬鹿工房 (UMA SHIKA KOBO) - Portal Script
-console.log('UMA SHIKA KOBO Portal Loaded.');
-
-// 将来的にゲームの追加や動的なUI制御を行うための拡張ポイント
-document.addEventListener('DOMContentLoaded', () => {
-    console.log('Welcome to the UMA SHIKA KOBO universe.');
-});
